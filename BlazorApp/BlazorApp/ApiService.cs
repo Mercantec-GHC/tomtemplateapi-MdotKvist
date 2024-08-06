e399838f70319b7cd1f1999800dc6360fdbf1dfd
MdotKvist <Kvist339@gmail.com>
2024-08-06 14:57:49 +0200

    Starte API projekt

---
+++ BlazorApp/BlazorApp/ApiService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace BlazorApp
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<JsonContent>> GetData()
+        {
+            var response = await _httpClient.GetAsync("https://magsapi.onrender.com/api/miles95");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<JsonContent>>(content);
+        }
+    }
+}
