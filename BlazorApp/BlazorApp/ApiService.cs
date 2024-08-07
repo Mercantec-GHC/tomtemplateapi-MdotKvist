3d33abacd8b3d2f4b791e9372a844c7416c9a22b
MdotKvist <Kvist339@gmail.com>
2024-08-07 12:41:11 +0200

    Deserialized the json data to at list

--- BlazorApp/BlazorApp/ApiService.cs
+++ BlazorApp/BlazorApp/ApiService.cs
@@ -1,26 +1,26 @@
-﻿using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text.Json;
+﻿//using System.Net.Http;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using System.Text.Json;
 
-namespace BlazorApp
-{
-    public class ApiService
-    {
-        private readonly HttpClient _httpClient;
+//namespace BlazorApp
+//{
+//    public class ApiService
+//    {
+//        private readonly HttpClient _httpClient;
 
-        public ApiService(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-        }
+//        public ApiService(HttpClient httpClient)
+//        {
+//            _httpClient = httpClient;
+//        }
 
-        public async Task<List<JsonContent>> GetData()
-        {
-            var response = await _httpClient.GetAsync("https://magsapi.onrender.com/api/miles95");
-            response.EnsureSuccessStatusCode();
+//        public async Task<List<JsonContent>> GetData()
+//        {
+//            var response = await _httpClient.GetAsync("https://magsapi.onrender.com/api/miles95");
+//            response.EnsureSuccessStatusCode();
 
-            var content = await response.Content.ReadAsStringAsync();
-            return JsonSerializer.Deserialize<List<JsonContent>>(content);
-        }
-    }
-}
+//            var content = await response.Content.ReadAsStringAsync();
+//            return JsonSerializer.Deserialize<List<JsonContent>>(content);
+//        }
+//    }
+//}
--- BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898746ab3bb1d13594dec48edad557a251625116")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e399838f70319b7cd1f1999800dc6360fdbf1dfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
