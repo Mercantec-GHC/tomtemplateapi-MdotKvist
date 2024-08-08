be0c1355f2804f038dc52656ad88a372f0d74b01
MdotKvist <Kvist339@gmail.com>
2024-08-08 14:45:26 +0200

    Made Diesel data individually and mate the homepage show both datas

---
+++ BlazorApp/BlazorApp/ApiClass.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp
+{
+    public class ApiUrl
+    {
+        public string Diesel { get; set; }
+        public string Miles { get; set; }
+    }
+}
---
+++ BlazorApp/BlazorApp/ApiData.cs
@@ -0,0 +1,10 @@
+﻿using BlazorApp;
+
+public class ApiData
+{
+    public string DieselDataJson { get; set; }
+    public string MilesDataJson { get; set; }
+
+    public List<Data> DieselData { get; set; }
+    public List<Data> MilesData { get; set; }
+}
